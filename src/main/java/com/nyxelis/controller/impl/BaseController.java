0c0e4bcf16126a8c21959266b2f90759da5d88c4
Huseyin DOL <huseyindol@gmail.com>
2025-10-03 22:55:46 +0300

    response data standart

---
+++ src/main/java/com/nyxelis/controller/impl/BaseController.java
@@ -0,0 +1,12 @@
+package com.nyxelis.controller.impl;
+
+import com.nyxelis.entity.RootEntityResponse;
+
+public class BaseController {
+    public <T> RootEntityResponse<T> ok(T data){
+        return RootEntityResponse.ok(data);
+    }
+    public <T> RootEntityResponse<T> error(String message){
+        return RootEntityResponse.error(message);
+    }
+}
